225c4adca7ab1fbddd112e66c7dcb33792053ce2
PittnoiR <omargarza394@gmail.com>
2024-01-15 20:36:37 -0600

    menu features terminado con swich y no if

--- js/index.js
+++ js/index.js
@@ -2,35 +2,95 @@ const landing = {
     burger: document.getElementById('burger'),
     nav :document.getElementById('navID'),
     logo:document.querySelector('.logo_nav'),
-    
+    slide:document.querySelectorAll('.Tremnu__contain--nav p'),
+    containSlide: document.querySelectorAll('.Tremnu__contain--slides div'),
+    social: document.querySelector('.foter div'),
     
 }
-console.log(landing.logo)
 
+landing.containSlide.forEach(contain =>{
+    console.log(contain)
+    
 
-landing.burger.addEventListener('click',()=>{
-    landing.nav.classList.toggle('deactive')
-    if(landing.burger.getAttribute('src') == './images/icon-hamburger.svg'){
-        landing.burger.src =  './images/icon-close.svg'
-        console.log('SI ')
-        console.log(landing.burger.src)
-        document.querySelector('body').style.overflow = 'hidden';
-        landing.logo.id = "logo";
-    }
-    else{
-        landing.burger.src = './images/icon-hamburger.svg'
-        document.querySelector('body').style.overflow = 'scroll'; 
-        console.log('no')
-        console.log(landing.burger.src)
-        landing.logo.id = ' '
-    }
-    
-    
-    
-    
-    
-   
-    
-    
-    
 })
+
+const [con1 ,con2,con3] = landing.containSlide
+const [uno ,dos ,tres] = landing.slide;
+
+
+
+
+
+
+
+
+
+const clickSlide = ()=>{
+
+
+    landing.slide.forEach(txt=>{
+    
+        txt.addEventListener('click',(e)=>{
+        
+            landing.slide.forEach(txt =>{
+                if(txt.className !== ''){
+                    txt.className = '';
+                };  
+                
+            });
+            txt.className = 'act';
+            
+            
+            let click = `${e.originalTarget.innerHTML}`;
+            console.log(click)
+           
+           
+                 switch (click){
+                case uno.innerHTML:
+                    con2.classList.add('deactive')
+                    con3.classList.add('deactive')
+                    con1.classList.remove('deactive')
+                    break
+                case dos.innerHTML:
+                    con1.classList.add('deactive')
+                    con3.classList.add('deactive')
+                    con2.classList.toggle('deactive')
+                    break
+                case tres.innerHTML:
+                    con1.classList.add('deactive')
+                    con2.classList.add('deactive')
+                    con3.classList.toggle('deactive')
+                    break
+            }
+        });
+    });
+
+
+}
+
+const clickMenu = ()=>{
+    landing.burger.addEventListener('click',()=>{
+        landing.nav.classList.toggle('deactive')
+        if(landing.burger.getAttribute('src') == './images/icon-hamburger.svg'){
+            landing.burger.src =  '/images/icon-close.svg';
+            document.querySelector('body').style.overflow = 'hidden';
+            landing.logo.id = "logo";
+            landing.social.style.position = 'fixed';
+            landing.social.style.bottom = '100px' ;   
+        }
+        else{
+            landing.burger.src = './images/icon-hamburger.svg'
+            document.querySelector('body').style.overflow = 'scroll'; 
+            landing.logo.id = ' '
+            landing.social.style.position = 'static';
+            
+            
+        }   
+        
+    })
+    
+
+}
+clickMenu()
+clickSlide()
+
