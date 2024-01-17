11be7e74064d9c41f85400dafc113d9bc32a74be
PittnoiR <omargarza394@gmail.com>
2024-01-17 16:02:31 -0600

    prieba overflow-x

---
+++ js/hover.js
@@ -0,0 +1,17 @@
+const bton2 = document.getElementById('bton2');
+const bton1 = bton2.previousElementSibling;
+var darkBlue = 'hsl(229, 31%, 21%)';
+var whiteocean = 'hsl(231, 69%, 60%)';
+
+bton2.addEventListener('mouseenter',()=>{
+    bton2.style.backgroundColor = whiteocean;
+    bton2.style.color = '#f0f0f0';
+    bton1.style.backgroundColor = '#f0f0f0'
+    bton1.style.color = darkBlue;
+})
+bton2.addEventListener('mouseleave',()=>{
+    bton2.style.backgroundColor = '';
+    bton2.style.color = '';
+    bton1.style.backgroundColor = ''
+    bton1.style.color = ''
+ })
